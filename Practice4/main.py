0596f8b8f5f6daa3870fcf86f8c3322985e4c4d6
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-05-24 01:27:31 +0300

    Refactor main.py prints

--- Practice4/main.py
+++ Practice4/main.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     test_2 = maximum_length_of_the_series_test(seq)
     test_3 = pokker_test(seq)
     test_4 = series_length_test(seq)
+    print(f'Generated 20,000-bit sequence (last 1 byte): ...{seq[-8::]}')
     if test_1 and test_2 and test_3 and test_4 == True:
-        print('The 20,000-bit sequence is sufficiently random')
+        print('The 20,000-bit generated sequence is random enough')
     else:
-        print('The bit sequence is rejected')
+        print('The generated bit sequence is rejected')
