9e06a9cbd569840d21bdc4e86ba99028f7c8f559
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-05-24 01:16:20 +0300

    Refactor code

--- Practice4/main.py
+++ Practice4/main.py
@@ -1,7 +1,9 @@
-from Practice4.key_standard.FIPS140_3 import *
+from Practice4.key_standard.FIPS140_3 import monobit_test, maximum_length_of_the_series_test, pokker_test, series_length_test
+from Practice4.bit_sequence.generate_bit_sequence import generate_bit_sequence
+
 
 if __name__ == '__main__':
-    seq = generate_sequence()
+    seq = generate_bit_sequence()
     test_1 = monobit_test(seq)
     test_2 = maximum_length_of_the_series_test(seq)
     test_3 = pokker_test(seq)
