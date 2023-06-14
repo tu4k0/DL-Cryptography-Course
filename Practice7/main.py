d6a13354080fabb21cfd32c61061fc2fb0fe4949
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-06-14 19:58:50 +0300

    Add elgamal signature and encryption demonstration

--- Practice7/main.py
+++ Practice7/main.py
@@ -1,8 +1,8 @@
-from Practice7.digital_signature.Elgamal import *
+from Practice7.digital_signature.Elgamal_signature import *
+from Practice7.encryption.Elgamal_encryption import *
 
 
-def main():
-    message = input('Enter message: ')
+def elgamal_signature(message):
     p, q = generate_random_prime_number()
     a, b = key_generation(p, q)
     print(f"\nKeys: \nPublic: {b}\nPrivate: {a}")
@@ -12,5 +12,22 @@ def main():
     print(f"\nVerification Status: {verification_status}")
 
 
+def elgamal_encryption(message):
+    blocks = create_blocks(message)
+    p, g = generate_random_prime_number()
+    a, b = key_generation(p, g)
+    print(f"\nKeys: \nPublic: {b}\nPrivate: {a}")
+    x, y = encrypt_message(blocks, p, g, b)
+    print(f"\nCiphertext:\n({x}, {y})")
+    m = decrypt_message(x, y, a, p)
+    print(f"\nPlaintext:\n{m}")
+
+
 if __name__ == '__main__':
-    main()
+    mode = input('Choose Elgamal mode (signature/encryption): ')
+    message = input('Enter message: ')
+    if mode == 'signature':
+        elgamal_signature(message)
+    elif mode == 'encryption':
+        elgamal_encryption(message)
+
