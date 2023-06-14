0b1ceae2e561ae3e0a0509088b616beaccd4e6ec
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-06-14 20:06:56 +0300

    Create Elgamal signature and encryption algorithms tests

---
+++ Practice7/tests/tests.py
@@ -0,0 +1,28 @@
+import unittest
+
+from Practice7.digital_signature import Elgamal_signature
+from Practice7.encryption import Elgamal_encryption
+
+
+class Tests(unittest.TestCase):
+
+    def test_Elgamal_signature(self):
+        input_text = 'Kyrylo'
+        p, q = Elgamal_signature.generate_random_prime_number()
+        a, b = Elgamal_signature.key_generation(p, q)
+        r, s, m = Elgamal_signature.sign_message(p, q, a, input_text)
+        verification_status = Elgamal_signature.check_signature(p, q, b, r, s, m)
+        self.assertTrue(True, verification_status)
+
+    def test_Elgamal_encryption(self):
+        message = 'Kyrylo'
+        blocks = Elgamal_encryption.create_blocks(message)
+        p, g = Elgamal_encryption.generate_random_prime_number()
+        a, b = Elgamal_encryption.key_generation(p, g)
+        x, y = Elgamal_encryption.encrypt_message(blocks, p, g, b)
+        m = Elgamal_encryption.decrypt_message(x, y, a, p)
+        self.assertEqual(message, m)
+
+
+if __name__ == "__main__":
+    unittest.main()
